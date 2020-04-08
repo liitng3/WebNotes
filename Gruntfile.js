5ed0ce9e6e1d6bc551a9bdfbd1d7ae1d4ab9786e
liitng3 <898260697@qq.com>
2020-04-08 16:20:01 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,5 +79,5 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-usemin');
-  grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint','copy', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin', 'htmlmin', 'imagemin']);
+  grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint','copy', 'useminPrepare', 'concat', 'uglify', 'cssmin','htmlmin', 'usemin',  'imagemin']);
 };
